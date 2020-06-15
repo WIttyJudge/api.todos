ac561c91fd648e974f8db0091cec3b9de8f56a9a
Ruslan <kros8925@gmail.com>
2020-06-15 21:49:53 +0300

    Remove hard-coded code by adding a read from .env file.

--- internal/app/todo-api/store/connection/postgres.go
+++ internal/app/todo-api/store/connection/postgres.go
@@ -1,20 +1,23 @@
 package connection
 
 import (
-	"fmt"
 	"log"
 
 	"github.com/jackc/pgx"
+	"github.com/wittyjudge/todo-api/internal/app/todo-api/utils"
 )
 
+// Connect inits connection to PostgreSQL
 func Connect() *pgx.Conn {
-	db := open("localhost", "5432", "postgres", "admin", "todo")
+	// db := open("localhost", "5432", "postgres", "admin", "todo")
+	conn := utils.GetEnv("POSTGRES_URL")
+	db := open(conn)
 	return db
 }
 
-func open(host, port, user, password, dbname string) *pgx.Conn {
-	connSrt := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s", host, port, user, password, dbname)
-	conn, err := pgx.ParseConnectionString(connSrt)
+func open(connStr string) *pgx.Conn {
+	// connSrt := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s", host, port, user, password, dbname)
+	conn, err := pgx.ParseConnectionString(connStr)
 	if err != nil {
 		log.Fatal(err)
 	}
