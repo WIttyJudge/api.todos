c9271bed4e7d1865deeedec5cf866dabaaef4edb
Ruslan <kros8925@gmail.com>
2020-07-22 21:32:31 +0300

    Create Validate func for validation

--- internal/app/todo-api/domain/entities/user.go
+++ internal/app/todo-api/domain/entities/user.go
@@ -1,12 +1,26 @@
 package entities
 
-import "time"
+import (
+	"time"
 
-// User is an entity of users database table
+	validation "github.com/go-ozzo/ozzo-validation"
+)
+
+// User is an entity of users database table.
+// Nickname and password is required.
+
 type User struct {
 	ID                int        `json:"id"`
 	Nickname          string     `json:"nickname"`
 	Password          string     `json:"password"`
 	EncryptedPassword string     `json:"-"`
 	CreateAt          *time.Time `json:"created_at"`
+}
+
+// Validate validates User struct fields.
+func (u User) Validate() error {
+	return validation.ValidateStruct(&u,
+		validation.Field(&u.Nickname, validation.Required),
+		validation.Field(&u.Password, validation.Required, validation.Length(6, 40)),
+	)
 }
